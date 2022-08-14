5173954a7ab5352e2e39c3969f55d9a008ca39c0
JONGMIN KIM <bellmin9321@gmail.com>
2022-08-15 01:43:12 +0900

    Add: ScreenProfileEdit 추가

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -3,13 +3,33 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { RootStackParamList } from "./types";
 import BottomTabNavigation from "./BottomTabNavigation";
+import ScreenProfileEdit from "../components/ScreenProfileEdit";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigation() {
   return (
-    <Stack.Navigator initialRouteName={"Main"} screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={"Main"} component={BottomTabNavigation} />
+    <Stack.Navigator
+      initialRouteName={"Main"}
+      screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name={"Main"}
+        component={BottomTabNavigation}
+        options={{
+          headerStyle: {
+            backgroundColor: "white",
+          },
+        }}
+      />
+      <Stack.Screen
+        name={"ProfileEdit"}
+        component={ScreenProfileEdit}
+        options={{
+          headerStyle: {
+            backgroundColor: "white",
+          },
+        }}
+      />
     </Stack.Navigator>
   );
 }
