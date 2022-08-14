5fe8a088ca008dac1b084a3c0cb26256080ac7cf
JONGMIN KIM <bellmin9321@gmail.com>
2022-08-15 01:41:12 +0900

    Add: ScreenHome에 TopTabNavigation 추가

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -2,72 +2,78 @@ import React from "react";
 import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { HomeTabParamList } from "./types";
-import ScreenHome from "../components/ScreenHome";
+import { BottomTabParamList } from "./types";
+import { IMAGES } from "../constant";
 import ScreenLive from "../components/ScreenLive";
 import ScreenLike from "../components/ScreenLike";
 import ScreenTalk from "../components/ScreenTalk";
-import ScreenProfile from "../components/ScreenProfile";
-import { IMAGES } from "../constant";
+import ScreenProfile from "../components/ScreenProfileEdit";
+import TopTabNavigation from "./TopTabNavigation";
 
-const Tab = createBottomTabNavigator<HomeTabParamList>();
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 function BottomTabNavigation() {
   return (
-    <Tab.Navigator
+    <BottomTab.Navigator
       initialRouteName="Home"
       screenOptions={{
-        headerShown: false,
         tabBarShowLabel: false,
         tabBarActiveTintColor: "#1A1A1A",
         tabBarStyle: { height: 50 },
       }}>
-      <Tab.Screen
+      <BottomTab.Screen
         name="Home"
-        component={ScreenHome}
+        component={TopTabNavigation}
         options={{
           tabBarIcon: ({ color }) => {
             return <Image source={IMAGES.HOME} style={{ tintColor: color }} />;
           },
+          header: () => null,
         }}
       />
-      <Tab.Screen
+      <BottomTab.Screen
         name="Live"
         component={ScreenLive}
         options={{
           tabBarIcon: ({ color }) => {
             return <Image source={IMAGES.LIVE} style={{ tintColor: color }} />;
           },
+          header: () => null,
         }}
       />
-      <Tab.Screen
+      <BottomTab.Screen
         name="Like"
         component={ScreenLike}
         options={{
           tabBarIcon: ({ color }) => {
             return <Image source={IMAGES.LIKE} style={{ tintColor: color }} />;
           },
+          header: () => null,
         }}
       />
-      <Tab.Screen
+      <BottomTab.Screen
         name="Talk"
         component={ScreenTalk}
         options={{
           tabBarIcon: ({ color }) => {
             return <Image source={IMAGES.TALK} style={{ tintColor: color }} />;
           },
+          header: () => null,
         }}
       />
-      <Tab.Screen
+      <BottomTab.Screen
         name="Profile"
         component={ScreenProfile}
         options={{
           tabBarIcon: ({ color }) => {
-            return <Image source={IMAGES.PROFILE} style={{ tintColor: color }} />;
+            return (
+              <Image source={IMAGES.PROFILE} style={{ tintColor: color }} />
+            );
           },
+          header: () => null,
         }}
       />
-    </Tab.Navigator>
+    </BottomTab.Navigator>
   );
 }
 
