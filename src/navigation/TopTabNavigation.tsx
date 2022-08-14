920cbc5a06d7babd3dfe326665aebd59bc9e890b
JONGMIN KIM <bellmin9321@gmail.com>
2022-08-15 01:49:50 +0900

    Feat: Setting 이미지 추가 및 클릭 시 ProfileEdit 페이지 이동 - navigation type 생성

--- src/navigation/TopTabNavigation.tsx
+++ src/navigation/TopTabNavigation.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-import { TopTabParamList } from "./types";
+import { RootStackParamList, TopTabParamList } from "./types";
 import { IMAGES } from "../constant";
 import ScreenHome from "../components/ScreenHome";
+import { StackNavigationProp } from "@react-navigation/stack";
 
 const NearScreen = () => {
   return (
@@ -24,58 +25,84 @@ const LiveScreen = () => {
 
 const TopTab = createMaterialTopTabNavigator<TopTabParamList>();
 
-function TopTabNavigation() {
+type TopTabNavigationProp = StackNavigationProp<RootStackParamList, "Main">;
+
+type Props = {
+  navigation: TopTabNavigationProp;
+};
+
+function TopTabNavigation({ navigation }: Props) {
+  const moveProfileEditPage = () => {
+    navigation.navigate("ProfileEdit");
+  };
+
   return (
-    <TopTab.Navigator
-      initialRouteName="MainFeed"
-      screenOptions={{
-        tabBarPressColor: "transparent",
-        tabBarActiveTintColor: "#1A1A1A",
-        tabBarInactiveTintColor: "#CCCCCC",
-        tabBarStyle: {
-          height: 56,
-          paddingRight: "40%",
-          justifyContent: "center",
-        },
-        tabBarIconStyle: {
-          marginLeft: 8,
-          top: 5,
-          alignItems: "center",
-          justifyContent: "center",
-        },
-        tabBarLabelStyle: {
-          fontSize: 20,
-          width: "100%",
-          fontWeight: "bold",
-        },
-        tabBarIndicatorStyle: {
-          backgroundColor: "black",
-          marginLeft: 8,
-          width: 70,
-        },
-      }}>
-      <TopTab.Screen
-        name="MainFeed"
-        component={ScreenHome}
-        options={{
-          tabBarIcon: ({ color }) => {
-            return <Image source={IMAGES.GLAM} style={{ tintColor: color }} />;
+    <>
+      <TouchableOpacity style={styles.settingBtn} onPress={moveProfileEditPage}>
+        <Image source={IMAGES.SETTING} />
+      </TouchableOpacity>
+      <TopTab.Navigator
+        initialRouteName="MainFeed"
+        screenOptions={{
+          tabBarPressColor: "transparent",
+          tabBarActiveTintColor: "#1A1A1A",
+          tabBarInactiveTintColor: "#CCCCCC",
+          tabBarStyle: {
+            height: 56,
+            paddingRight: "40%",
+            justifyContent: "center",
           },
-          tabBarShowLabel: false,
-        }}
-      />
-      <TopTab.Screen
-        name="Near"
-        component={NearScreen}
-        options={{ tabBarLabel: "근처" }}
-      />
-      <TopTab.Screen
-        name="Feed"
-        component={LiveScreen}
-        options={{ tabBarLabel: "라이브" }}
-      />
-    </TopTab.Navigator>
+          tabBarIconStyle: {
+            marginLeft: 8,
+            top: 5,
+            alignItems: "center",
+            justifyContent: "center",
+          },
+          tabBarLabelStyle: {
+            fontSize: 20,
+            width: "100%",
+            fontWeight: "bold",
+          },
+          tabBarIndicatorStyle: {
+            backgroundColor: "black",
+            marginLeft: 8,
+            width: 70,
+          },
+        }}>
+        <TopTab.Screen
+          name="MainFeed"
+          component={ScreenHome}
+          options={{
+            tabBarIcon: ({ color }) => {
+              return (
+                <Image source={IMAGES.GLAM} style={{ tintColor: color }} />
+              );
+            },
+            tabBarShowLabel: false,
+          }}
+        />
+        <TopTab.Screen
+          name="Near"
+          component={NearScreen}
+          options={{ tabBarLabel: "근처" }}
+        />
+        <TopTab.Screen
+          name="Feed"
+          component={LiveScreen}
+          options={{ tabBarLabel: "라이브" }}
+        />
+      </TopTab.Navigator>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  settingBtn: {
+    position: "absolute",
+    top: 20,
+    right: 12,
+    zIndex: 1,
+  },
+});
 
 export default TopTabNavigation;
